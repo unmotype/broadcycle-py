139513c4f7609f8c59c9d6a4e8d4124d6c66e694
evmgolf <evmgolf@github.com>
2022-11-30 12:10:51 -0800

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='broadcycle',
+    version='0.0.1',
+    install_requires=[
+        'aioredis',
+        'aioipfs',
+        'async-timeout<4.0,>=3.0',
+        'fastapi',
+        'spacy',
+    ],
+)
